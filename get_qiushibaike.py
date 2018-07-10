d87bafacde491c43d5f72b013340bfa3715d2e3e
fanzehua <316679581@qq.com>
2018-07-10 14:14:48 +0800

    优化代码写法,提取相同代码块

--- get_qiushibaike.py
+++ get_qiushibaike.py
@@ -5,30 +5,32 @@ from bs4 import BeautifulSoup
 import re
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-baseUrl = 'https://www.qiushibaike.com'
-spilUrl = 'https://www.qiushibaike.com/8hr/page/'
-file = open('/Users/smt/Desktop/test.txt', 'a+')
-for i in range(5):
-    if i == 0:
-        r = requests.get(baseUrl, headers)
-        soup = BeautifulSoup(r.text, 'lxml')
+class Qiushi(object):
+    def __init__(self):
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+        self.baseUrl = 'https://www.qiushibaike.com'
+        self.spilUrl = 'https://www.qiushibaike.com/8hr/page/'
+        self.file = open('/Users/smt/Desktop/test.txt', 'a+')
 
-        divs = soup.find_all('div', re.compile('^article'))
-        for div in divs:
-            if div.find_all('div','thumb'):
-                continue
-            elif div.find_all('div', 'content'):
-                file.write(div.span.get_text())
-                file.write('-----------------')
-    else:
-        r = requests.get(spilUrl + str(i), headers)
-        soup = BeautifulSoup(r.text, 'lxml')
+    def spider(self, html):
+            soup = BeautifulSoup(html, 'lxml')
+            divs = soup.find_all('div', re.compile('^article'))
+            for div in divs:
+                if div.find_all('div','thumb'):
+                    continue
+                elif div.find_all('div', 'content'):
+                    self.file.write(div.span.get_text())
+                    self.file.write('-----------------')
 
-        divs = soup.find_all('div', re.compile('^article'))
-        for div in divs:
-            if div.find_all('div','thumb'):
-                continue
-            elif div.find_all('div', 'content'):
-                file.write(div.span.get_text())
-                file.write('-----------------\n' )
+    def craw(self):
+        for i in range(5):
+            if i == 0:
+                r = requests.get(self.baseUrl, self.headers)
+                self.spider(r.text)
+            else:
+                r = requests.get(self.spilUrl + str(i), self.headers)
+                self.spider(r.text)
+
+if __name__ == '__main__':
+    obj_spider = Qiushi()
+    obj_spider.craw()
